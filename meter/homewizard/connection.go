fca5a582c1e60c4109d1f649f20ed0b971360bf6
Roeland <Roeland.Lutters@telenet.be>
2024-07-10 14:12:49 +0200

    import api

--- meter/homewizard/connection.go
+++ meter/homewizard/connection.go
@@ -11,6 +11,7 @@ import (
 	"github.com/evcc-io/evcc/util"
 	"github.com/evcc-io/evcc/util/request"
 	"github.com/evcc-io/evcc/util/transport"
+	"github.com/evcc-io/evcc/api"
 )
 
 // Connection is the homewizard connection
